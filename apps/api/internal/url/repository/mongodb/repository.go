d261ac531703394345fe21e76d5f880ec8aadc3f
tapadar13 <tapadar@prodgain.com>
2026-07-10 10:47:05 +0530

    feat: add URL destination update repository operation

--- apps/api/internal/url/repository/mongodb/repository.go
+++ apps/api/internal/url/repository/mongodb/repository.go
@@ -20,9 +20,14 @@ type findOneCollection interface {
 	FindOne(ctx context.Context, filter any, opts ...options.Lister[options.FindOneOptions]) *mongo.SingleResult
 }
 
+type findOneAndUpdateCollection interface {
+	FindOneAndUpdate(ctx context.Context, filter any, update any, opts ...options.Lister[options.FindOneAndUpdateOptions]) *mongo.SingleResult
+}
+
 type collection interface {
 	insertOneCollection
 	findOneCollection
+	findOneAndUpdateCollection
 }
 
 type Repository struct {
@@ -113,4 +118,52 @@ func (r *Repository) FindByShortCode(ctx context.Context, shortCode string) (url
 	}
 
 	return record, nil
+}
+
+func (r *Repository) UpdateLongURL(ctx context.Context, params urlmodel.UpdateLongURLParams) (urlmodel.URL, error) {
+	if r == nil || r.collection == nil {
+		return urlmodel.URL{}, errors.New("MongoDB URL collection is required")
+	}
+
+	normalizedShortCode, err := shortcode.Normalize(params.ShortCode)
+	if err != nil {
+		return urlmodel.URL{}, err
+	}
+
+	normalizedLongURL, err := urlmodel.NormalizeLongURL(params.LongURL)
+	if err != nil {
+		return urlmodel.URL{}, err
+	}
+
+	if params.UpdatedAt.IsZero() {
+		return urlmodel.URL{}, urlmodel.ErrTimestampRequired
+	}
+
+	updatedAt := params.UpdatedAt.UTC()
+	result := r.collection.FindOneAndUpdate(
+		ctx,
+		bson.D{{Key: "short_code", Value: normalizedShortCode}},
+		bson.D{{Key: "$set", Value: bson.D{
+			{Key: "url", Value: normalizedLongURL},
+			{Key: "updated_at", Value: updatedAt},
+		}}},
+		options.FindOneAndUpdate().SetReturnDocument(options.After),
+	)
+	if result == nil {
+		return urlmodel.URL{}, errors.New("update URL by short code: missing result")
+	}
+
+	var doc urlDocument
+	if err := result.Decode(&doc); errors.Is(err, mongo.ErrNoDocuments) {
+		return urlmodel.URL{}, fmt.Errorf("%w: %w", urlmodel.ErrNotFound, err)
+	} else if err != nil {
+		return urlmodel.URL{}, fmt.Errorf("update URL by short code: %w", err)
+	}
+
+	updated, err := doc.toDomain()
+	if err != nil {
+		return urlmodel.URL{}, err
+	}
+
+	return updated, nil
 }
