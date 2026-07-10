8ec2f805f6816dc94ffeeda1953adb9706794e52
tapadar13 <tapadar@prodgain.com>
2026-07-10 11:04:01 +0530

    feat: add URL destination update service

---
+++ apps/api/internal/url/service/update.go
@@ -0,0 +1,74 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/tapadar13/url-shortener/apps/api/internal/shortcode"
+	urlmodel "github.com/tapadar13/url-shortener/apps/api/internal/url"
+)
+
+type UpdateRepository interface {
+	UpdateLongURL(ctx context.Context, params urlmodel.UpdateLongURLParams) (urlmodel.URL, error)
+}
+
+type UpdateOptions struct {
+	Now func() time.Time
+}
+
+type UpdateService struct {
+	repository UpdateRepository
+	now        func() time.Time
+}
+
+type UpdateParams struct {
+	ShortCode string
+	LongURL   string
+}
+
+func NewUpdateService(repository UpdateRepository, options UpdateOptions) (*UpdateService, error) {
+	if repository == nil {
+		return nil, ErrRepositoryRequired
+	}
+
+	if options.Now == nil {
+		options.Now = time.Now
+	}
+
+	return &UpdateService{
+		repository: repository,
+		now:        options.Now,
+	}, nil
+}
+
+func (s *UpdateService) UpdateLongURL(ctx context.Context, params UpdateParams) (urlmodel.URL, error) {
+	if s == nil || s.repository == nil {
+		return urlmodel.URL{}, ErrRepositoryRequired
+	}
+
+	if s.now == nil {
+		s.now = time.Now
+	}
+
+	normalizedShortCode, err := shortcode.Normalize(params.ShortCode)
+	if err != nil {
+		return urlmodel.URL{}, err
+	}
+
+	normalizedLongURL, err := urlmodel.NormalizeLongURL(params.LongURL)
+	if err != nil {
+		return urlmodel.URL{}, err
+	}
+
+	updated, err := s.repository.UpdateLongURL(ctx, urlmodel.UpdateLongURLParams{
+		ShortCode: normalizedShortCode,
+		LongURL:   normalizedLongURL,
+		UpdatedAt: s.now().UTC(),
+	})
+	if err != nil {
+		return urlmodel.URL{}, fmt.Errorf("update URL: %w", err)
+	}
+
+	return updated, nil
+}
